b4b92487c39928a31f954e5a9da50818ba1967b6
Miguel Ruiz tome <miguel.tome091@gmail.com>
2023-05-31 16:21:51 +0200

    src

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import * as SQLite from "expo-sqlite";
 import { db, createTables, selectAllNotes, insertNote } from "./database";
 import { StatusBar } from "expo-status-bar";
-import CreateNote from "./scr/components/CreateNote";
+import CreateNote from "./src/components/CreateNote";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
